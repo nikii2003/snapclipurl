8ed8b5c4236c32362497fb1269d13f18e2d802c0
Nikita kadiwike <ninavenikita492@gmail.com>
2023-11-18 08:35:11 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from 'dotenv'
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+
+const connectDB = async () =>{
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    if(conn){
+        console.log("mongodb connected");
+    }
+};
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
+})
+
+
+
+
