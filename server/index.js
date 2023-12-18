ff93b72b7bdec7fc55af77e3b7bae515a5a7741c
Nikita kadiwike <ninavenikita492@gmail.com>
2023-12-18 12:46:22 +0530

    hostiong process

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import Link from "./models/Link.js";
+import path from 'path';
 dotenv.config();
 const app = express();
 app.use(express.json());
+
+const __dirname = path.resolve();
 const connectDB = async () =>{
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     if(conn){
@@ -80,6 +83,18 @@ app.get('/api/links',async(req,res)=>{
    message : "Link fetch Successfully"
   })
 })
+
+if(process.env.NODE_ENV = 'production'){
+  app.use(express.static(path.join(__dirname, '..','client','build')));
+
+
+  //* all get request
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, '..', 'client','build','index.html'))
+  });
+}
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
